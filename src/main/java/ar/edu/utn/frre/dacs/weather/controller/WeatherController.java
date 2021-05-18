a945de926bb9eabb0164e15129d19ddc2f262d29
jorgevillaverde <jorge.villaverde@toucansoft.io>
2021-05-18 16:18:00 -0300

    feat: add create weather endpoint
    Signed-off-by: jorgevillaverde <jorge.villaverde@toucansoft.io>

--- src/main/java/ar/edu/utn/frre/dacs/weather/controller/WeatherController.java
+++ src/main/java/ar/edu/utn/frre/dacs/weather/controller/WeatherController.java
@@ -20,6 +20,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -47,5 +49,11 @@ public class WeatherController {
 	@GetMapping("")
 	public ResponseEntity<List<Weather>> getWeathers() {
 		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@PostMapping("")
+	public ResponseEntity<Weather> createWeather(@RequestBody Weather request) {
+		Weather created = repository.save(request);
+		return ResponseEntity.ok(created);
 	}
 }
--- src/main/java/ar/edu/utn/frre/dacs/weather/model/Weather.java
+++ src/main/java/ar/edu/utn/frre/dacs/weather/model/Weather.java
@@ -6,6 +6,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -21,6 +23,7 @@ import lombok.Data;
 public class Weather {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "id")
 	private Long id;
 	
